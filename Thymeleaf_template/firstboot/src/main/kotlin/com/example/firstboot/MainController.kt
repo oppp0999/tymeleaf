7c2f006f5f063442847242667ed7915345928519
oppp0999 <oppp0999@naver.com>
2022-11-08 20:40:40 +0900

    20221108_1

---
+++ Thymeleaf_template/firstboot/src/main/kotlin/com/example/firstboot/MainController.kt
@@ -0,0 +1,25 @@
+package com.example.firstboot
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+data class Product(
+    val name: String,
+    val price: Int
+)
+
+@Controller
+class MainController {
+    @GetMapping("/")
+    fun index(model: Model) : String {
+        model.addAttribute("name", "vlad")
+        model.addAttribute("products",
+            listOf(
+                Product("Apple", 3),
+                Product("Strawberry", 5),
+                Product("Potato", 1)
+            ))
+        return "index"
+    }
+}
